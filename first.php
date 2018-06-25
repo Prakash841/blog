1211c910bf087bdc3adcce5d7f76d18728f54927
Prakash <pbsc20@gmail.com>
2018-06-25 03:55:41 -0700

    Add files via upload

---
+++ first.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+</!DOCTYPE html>
+<html>
+<head>
+	<title>hi</title>
+</head>
+<body>
+<table border="1">
+	<th>
+		1
+	</th>
+	<th>
+		2
+	</th>
+	<th>3
+	</th>
+	<tr>
+		<td>fds</td>
+		<td>dg</td>
+		<td>dfgf</td>
+	</tr>
+	<tr>
+		<td>fdfd</td>
+		<td>fdf</td>
+		<td>fdf</td>
+	</tr>
+</table>
+</body>
+</html>
+
